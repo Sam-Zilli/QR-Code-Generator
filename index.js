2254db338d550452678bf04187c746e12e63fe4e
Sam Zilli <sam@Sams-iMac.local>
2023-11-07 13:06:50 -0500

    complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import inquirer from "inquirer";
 import qr from 'qr-image';
-import fs from 'fs';
+import fs, { open, close, appendFile } from 'node:fs';
+
 
 const getUrl = [ { 
     type : "input", 
@@ -8,12 +9,38 @@ const getUrl = [ {
     message : "Whats the URL you want a QR image for?" 
 } ]
 
+function closeFd(fd) {
+    close(fd, (err) => {
+      if (err) throw err;
+    });
+  }
+
 // npm package for getting user input 
 inquirer 
     .prompt(getUrl) 
     .then((answers) => 
-        { console.log("Here ya go...");
-         console.log(qr.imageSync(answers.urlRequested)) }) 
+        { 
+        console.log("Here ya go...");
+
+        open('URL.txt', 'a', (err, fd) => {
+            if (err) throw err;
+          
+            try {
+              appendFile(fd, ("\n" + answers.urlRequested), 'utf8', (err) => {
+                closeFd(fd);
+                if (err) throw err;
+              });
+            } catch (err) {
+              closeFd(fd);
+              throw err;
+            }
+          });
+        
+        var qrImg = qr.image(answers.urlRequested, { type: 'png' });
+        qrImg.pipe(fs.createWriteStream('qr_img.png'));
+        
+
+        }) 
     .catch((error) => { if (error.isTtyError) 
         { "Sorry, something went wrong." } 
         else 
